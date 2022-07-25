d62023a719bff7d9afe747b09d3752f86f399f10
brubrandaom <85810656+brubrandaom@users.noreply.github.com>
2022-07-25 01:05:52 -0300

    Delete q3.js

--- q3.js
+++
@@ -1,6 +0,0 @@
-//Faça um programa que leia o ano de nascimento de uma pessoa,
-//calcule a idade dela e depois mostre se ela pode ou não votar.
-
-var ano=2001
-voto = (ano) => {return 2022-ano>=16? "PODE VOTAR!":"NAO PODE VOTAR!"}
-console.log(voto(ano));
